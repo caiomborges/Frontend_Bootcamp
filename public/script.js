8552aff2a68360f6acf56a36a5a34812f9f07d28
= <=>
2024-01-31 21:23:15 -0300

    added task 4

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ form_button.addEventListener("click", () => {
   if (name_input == ""){
     alert("Name must be filled out!");
   }
-})
+});
 
 var list_button = document.getElementById("list-button");
 list_button.addEventListener("click", () => {
@@ -17,4 +17,19 @@ list_button.addEventListener("click", () => {
   li.addEventListener("click", () => {
     ul.removeChild(li);
   })
-})
+});
+
+var list_button = document.getElementById("list-button2");
+list_button.addEventListener("click", () => {
+  var ul = document.getElementById("list2");
+  var li = document.createElement("li");
+  var children = ul.children.length + 1;
+  li.setAttribute("id", "element2"+children);
+  li.setAttribute("class", "list_item");
+  li.appendChild(document.createTextNode("Mussum Ipsum, cacilds vidis litro abertis.  Nulla id gravida magna, ut semper sapien. Pra lá, depois divoltis porris, paradis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Bota 1 metro de cachacis aí pra viagem!"+children));
+  ul.appendChild(li);
+  var item = document.getElementById("element2"+children);
+  li.addEventListener("click", () => {
+    ul.removeChild(li);
+  })
+});
