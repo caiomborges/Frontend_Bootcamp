117f76311aced61e094637807560dd1c93fe1061
= <=>
2024-03-06 12:00:55 -0300

    feat: add counter hook

--- src/index.js
+++ src/index.js
@@ -2,23 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App.js"
 
-//this is the data that will never appear anywhere
-var pokemons = [ {
-    name: "Charizard",
-    type: "fire"
-},
-{
-    name: "Bulbasaur",
-    type: "grass"
-}, {
-    name: "Squirtle",
-    type: "water"
-}, {
-    name: "Pikachu",
-    type: "electric"
-}
-]
-
 ReactDOM.render(
     <App />,
     document.getElementById("root")
--- src/molecules/Counter.jsx
+++ src/molecules/Counter.jsx
@@ -1,1 +1,15 @@
-import React from 'react'
+import React, { useReducer } from "react";
+import reducer from '../hooks/CounterHook'
+
+const initialState = { count: 0 };
+
+export default function Counter() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+      <>
+        Count: {state.count}
+        <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      </>
+    );
+}
